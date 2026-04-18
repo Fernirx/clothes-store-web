b72514d3ce59857a3d9595548f0c188fc6b3317d
Nguyen Tri Cuong <nguyentricuong1504@gmail.com>
2026-04-18 23:18:47 +0700

    sửa lại thêm product và variant hoàn chỉnh

--- src/pages/products/ProductList.js
+++ src/pages/products/ProductList.js
@@ -132,7 +132,7 @@ export default function ProductList() {
                     <td>
                       <div style={{ display: 'flex', gap: '8px' }}>
                         {/* ĐÃ SỬA: Thay item thành p ở dòng dưới đây */}
-                        <button className="btn btn-sm" onClick={() => navigate(`/products/form/${p.id}`, { state: { productData: p } })} >Sửa</button>
+                        <button className="btn btn-sm" onClick={() => navigate(`/products/form/edit/${p.id}`, { state: { productData: p } })} >Sửa</button>
 
                         <button
                           className="btn btn-sm"
